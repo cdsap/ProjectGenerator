1ea166b308ef9aa0e0580a76d62c8a95a812279e
Inaki Villar <inaki.seri@gmail.com>
2023-12-15 21:28:07 +0100

    prepare new relase

--- project-generator/build.gradle.kts
+++ project-generator/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.20"
+    `maven-publish`
+    `signing`
 }
 
 group = "io.github.cdsap"
-version = "0.1.0"
+version = "0.1.5"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
@@ -12,4 +14,83 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+configure<JavaPluginExtension> {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Snapshots"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+
+            credentials {
+                username = System.getenv("USERNAME_SNAPSHOT")
+                password = System.getenv("PASSWORD_SNAPSHOT")
+            }
+        }
+        maven {
+            name = "Release"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = System.getenv("USERNAME_SNAPSHOT")
+                password = System.getenv("PASSWORD_SNAPSHOT")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("libPublication") {
+            from(components["java"])
+            artifactId = "projectgenerator"
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                scm {
+                    connection.set("scm:git:git://github.com/cdsap/ProjectGenerator/")
+                    url.set("https://github.com/cdsap/ProjectGenerator/")
+                }
+                name.set("projectgenerator")
+                url.set("https://github.com/cdsap/ProjectGenerator/")
+                description.set(
+                    "Returns graph metrics by project"
+                )
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("cdsap")
+                        name.set("Inaki Villar")
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (extra.has("signing.keyId")) {
+    afterEvaluate {
+        configure<SigningExtension> {
+            (
+                extensions.getByName("publishing") as
+                    PublishingExtension
+                ).publications.forEach {
+                    sign(it)
+                }
+        }
+    }
 }
