d579f2ba0f8ea104326404da4f9ea4680dde85f5
Iñaki Villar <inaki.seri@gmail.com>
2024-07-15 11:39:53 -0700

    Updating readme and default versions (#25)
    * updating readme
    
    * removing minify from android library

--- project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
+++ project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
@@ -1,6 +1,6 @@
 package io.github.cdsap.projectgenerator.model
 
 data class Versions(
-    val agp: String = "8.1.4",
-    val kgp: String = "1.9.20"
+    val agp: String = "8.5.1",
+    val kgp: String = "1.9.24"
 )
