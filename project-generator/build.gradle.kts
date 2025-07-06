f972b41ab1f2afcdc0087a251a7468fee64ccb04
Inaki Villar <inaki.seri@gmail.com>
2025-07-06 15:44:51 -0700

    migrating to vanniktech.maven.publish

--- project-generator/build.gradle.kts
+++ project-generator/build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     kotlin("jvm") version "2.2.0"
     `maven-publish`
     `signing`
+    id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
 group = "io.github.cdsap"
 version = "0.2.1"
 
 dependencies {
-    implementation("com.squareup:kotlinpoet:2.1.0")
     testImplementation(platform("org.junit:junit-bom:5.11.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(gradleTestKit())
@@ -18,71 +18,37 @@ tasks.test {
     useJUnitPlatform()
 }
 
-configure<JavaPluginExtension> {
-    withJavadocJar()
-    withSourcesJar()
-}
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("io.github.cdsap", "projectgenerator", "0.2.1")
 
-publishing {
-    repositories {
-        maven {
-            name = "Snapshots"
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-            credentials {
-                username = System.getenv("USERNAME_SNAPSHOT")
-                password = System.getenv("PASSWORD_SNAPSHOT")
+    pom {
+        scm {
+            connection.set("scm:git:git://github.com/cdsap/ProjectGenerator/")
+            url.set("https://github.com/cdsap/ProjectGenerator/")
+        }
+        name.set("projectgenerator")
+        url.set("https://github.com/cdsap/ProjectGenerator/")
+        description.set(
+            "Android and JVM Gradle Projects generator"
+        )
+        licenses {
+            license {
+                name.set("The MIT License (MIT)")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("repo")
             }
         }
-        maven {
-            name = "Release"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-
-            credentials {
-                username = System.getenv("USERNAME_SNAPSHOT")
-                password = System.getenv("PASSWORD_SNAPSHOT")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("libPublication") {
-            from(components["java"])
-            artifactId = "projectgenerator"
-            versionMapping {
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-            }
-            pom {
-                scm {
-                    connection.set("scm:git:git://github.com/cdsap/ProjectGenerator/")
-                    url.set("https://github.com/cdsap/ProjectGenerator/")
-                }
-                name.set("projectgenerator")
-                url.set("https://github.com/cdsap/ProjectGenerator/")
-                description.set(
-                    "Returns graph metrics by project"
-                )
-                licenses {
-                    license {
-                        name.set("The MIT License (MIT)")
-                        url.set("https://opensource.org/licenses/MIT")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("cdsap")
-                        name.set("Inaki Villar")
-                    }
-                }
+        developers {
+            developer {
+                id.set("cdsap")
+                name.set("Inaki Villar")
             }
         }
     }
 }
+
 
 if (extra.has("signing.keyId")) {
     afterEvaluate {
