e3de9624349bbbca51bbe6530958c5bd4a422e66
Inaki Villar <inaki.seri@gmail.com>
2025-07-21 19:54:46 -0700

    fix odd distribution for middle bottleneck

--- project-generator/src/test/kotlin/io/github/cdsap/projectgenerator/LayerDistributionTest.kt
+++ project-generator/src/test/kotlin/io/github/cdsap/projectgenerator/LayerDistributionTest.kt
@@ -1,8 +1,14 @@
 package io.github.cdsap.projectgenerator
 
+import io.github.cdsap.projectgenerator.model.ClassesPerModule
+import io.github.cdsap.projectgenerator.model.ClassesPerModuleType
 import io.github.cdsap.projectgenerator.model.Shape
+import io.github.cdsap.projectgenerator.model.TypeProjectRequested
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class LayerDistributionTest {
 
@@ -44,5 +50,28 @@ class LayerDistributionTest {
         val expected = listOf(100)
         val result = layerDistribution.get(Shape.FLAT)
         assertEquals(expected, result)
+    }
+
+    @ParameterizedTest
+    @EnumSource(Shape::class)
+    fun testCorrectAmountOfDistributionsIsReturned(shape: Shape) {
+
+        val layerDistribution = LayerDistribution(numberOfModules = 1800, numberOfLayers = 30)
+        val result = layerDistribution.get(shape)
+        if (shape == Shape.FLAT) {
+            assertEquals(1, result.size)
+
+        } else {
+            assertEquals(30, result.size)
+        }
+
+        val layerDistributionOdd = LayerDistribution(numberOfModules = 1801, numberOfLayers = 30)
+        val resultOdd = layerDistributionOdd.get(shape)
+        if (shape == Shape.FLAT) {
+            assertEquals(1, resultOdd.size)
+
+        } else {
+            assertEquals(30, result.size)
+        }
     }
 }
