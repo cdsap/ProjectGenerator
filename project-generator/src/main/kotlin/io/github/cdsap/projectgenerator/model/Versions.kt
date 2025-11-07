31a6112941962a5c16fd6574413c493c10e75617
Iñaki Villar <inaki.seri@gmail.com>
2025-11-07 17:12:58 +0100

    Update Versions.kt

--- project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
+++ project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
@@ -55,8 +55,8 @@ data class Testing(
 )
 
 data class Kotlin(
-    val kgp: String = "2.3.2",
-    val ksp: String = "2.2.20-2.0.4",
+    val kgp: String = "2.2.21",
+    val ksp: String = "2.3.2",
     val coroutines: String = "1.10.2",
     val kotlinTest: String = "2.2.21",
     val kotlinProcessor: KotlinProcessor = KotlinProcessor(),
