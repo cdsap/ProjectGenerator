827596b8b2b03f9ce5c125648cd20d7696181303
Inaki Villar <inaki.seri@gmail.com>
2025-11-10 10:52:11 -0800

    split test execution

--- project-generator/src/test/kotlin/io/github/cdsap/projectgenerator/SingleE2EValidationTest.kt
+++ project-generator/src/test/kotlin/io/github/cdsap/projectgenerator/SingleE2EValidationTest.kt
@@ -73,4 +73,54 @@ class SingleE2EValidationTest {
 
     }
 
+    @Test
+    fun sanityTestAgp() {
+        val modules = 50
+        val shape = Shape.RHOMBUS
+        ProjectGenerator(
+            modules = 50,
+            shape = shape,
+            language = Language.KTS,
+            typeOfProjectRequested = TypeProjectRequested.ANDROID,
+            classesPerModule = ClassesPerModule(ClassesPerModuleType.FIXED, 20),
+            versions = Versions(project = Project(jdk = "17")),
+            typeOfStringResources = TypeOfStringResources.LARGE,
+            layers = 5,
+            generateUnitTest = true,
+            gradle = GradleWrapper(Gradle.GRADLE_9_2_0),
+            path = tempDir.toFile().path,
+            projectName = "${shape.name.lowercase().capitalize()}_$modules"
+        ).write()
+        val filePath = File("$tempDir/${shape.name.lowercase().capitalize()}_${modules}/project_kts")
+        val result = GradleRunner.create()
+            .withProjectDir(filePath)
+            .withArguments("assembleDebug")
+            .build()
+        println(result.output)
+        assert(result.output.contains("BUILD SUCCESSFUL"))
+        val resultTest = GradleRunner.create()
+            .withProjectDir(filePath)
+            .withArguments("testDebugUnitTest")
+            .build()
+
+        val layerDir = NameMappings.layerName(0)
+        val moduleDir = NameMappings.moduleName("module_0_1")
+        assert(
+            File(
+                "$tempDir/${
+                    shape.name.lowercase().capitalize()
+                }_${modules}/project_kts/$layerDir/$moduleDir/build"
+            ).exists()
+                && File(
+                "$tempDir/${
+                    shape.name.lowercase().capitalize()
+                }_${modules}/project_kts/$layerDir/$moduleDir/build"
+            ).isDirectory
+        )
+
+        assert(resultTest.output.contains("BUILD SUCCESSFUL"))
+
+    }
+
+
 }
