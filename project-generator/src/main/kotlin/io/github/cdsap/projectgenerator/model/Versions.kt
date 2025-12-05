b28f733ea281cab8485a33c6866cda452533a034
Inaki Villar <inaki.seri@gmail.com>
2025-12-04 16:37:22 -0800

    update ksp version

--- project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
+++ project-generator/src/main/kotlin/io/github/cdsap/projectgenerator/model/Versions.kt
@@ -56,7 +56,7 @@ data class Testing(
 
 data class Kotlin(
     val kgp: String = "2.2.21",
-    val ksp: String = "2.3.2",
+    val ksp: String = "2.3.3",
     val coroutines: String = "1.10.2",
     val kotlinTest: String = "2.2.21",
     val kotlinProcessor: KotlinProcessor = KotlinProcessor(),
