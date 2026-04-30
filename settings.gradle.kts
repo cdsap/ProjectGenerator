77d31baf89ccb0925ac134a7a21f6dcac5dcbf41
Iñaki Villar <inaki.seri@gmail.com>
2026-04-30 15:39:52 -0700

    adding DV conf (#311)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("com.gradle.develocity") version "4.4.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.6.0"
+}
+
+develocity {
+    server = "https://community.develocity.cloud"
+    allowUntrustedServer = true
+    projectId.set("cdsap/ProjectGenerator")
+    buildScan {
+        uploadInBackground.set(false)
+    }
+}
+
 rootProject.name = "ProjectGenerator"
 include("project-generator")
 include("cli")
